e86060b3dca09e645d9754d1fcd6b00a9be7af6c
Andrew Westberg <andrewwestberg@gmail.com>
2020-12-20 18:39:29 +0000

    feat: Improve handshake error handling... actually check the response

--- src/protocols/handshake.rs
+++ src/protocols/handshake.rs
@@ -16,6 +16,15 @@ use serde_cbor::{de, ser, Value};
 
 use crate::{Agency, Protocol};
 
+const PROTOCOL_VERSION_1: i128 = 0x01;
+const PROTOCOL_VERSION_2: i128 = 0x02;
+const PROTOCOL_VERSION_SHELLEY: i128 = 0x03;
+const PROTOCOL_VERSION_SHELLEY2: i128 = 0x04;
+const PROTOCOL_VERSION_ALLEGRA: i128 = 0x05;
+const MIN_PROTOCOL_VERSION: i128 = PROTOCOL_VERSION_ALLEGRA;
+
+const MSG_ACCEPT_VERSION_MSG_ID: i128 = 1;
+
 #[derive(Debug)]
 pub enum State {
     Propose,
@@ -43,8 +52,11 @@ impl HandshakeProtocol {
     // Create the byte representation of MsgProposeVersions for sending to the server
     fn msg_propose_versions(&self, network_magic: u32) -> Vec<u8> {
         let mut payload_map: BTreeMap<Value, Value> = BTreeMap::new();
-        // protocol version 3 mapped to the network_magic value
-        payload_map.insert(Value::Integer(0x03), Value::Integer(network_magic as i128));
+        payload_map.insert(Value::Integer(PROTOCOL_VERSION_1), Value::Integer(network_magic as i128));
+        payload_map.insert(Value::Integer(PROTOCOL_VERSION_2), Value::Integer(network_magic as i128));
+        payload_map.insert(Value::Integer(PROTOCOL_VERSION_SHELLEY), Value::Integer(network_magic as i128));
+        payload_map.insert(Value::Integer(PROTOCOL_VERSION_SHELLEY2), Value::Array(vec![Value::Integer(network_magic as i128), Value::Bool(false)]));
+        payload_map.insert(Value::Integer(PROTOCOL_VERSION_ALLEGRA), Value::Array(vec![Value::Integer(network_magic as i128), Value::Bool(false)]));
 
         let message = Value::Array(vec![
             Value::Integer(0), // message_id
@@ -115,20 +127,72 @@ impl Protocol for HandshakeProtocol {
     }
 
     fn receive_data(&mut self, data: Vec<u8>) {
-        if data.len() != 8 {
-            // some payload error
-            let cbor_value: Value = de::from_slice(&data[..]).unwrap();
-            match self.find_error_message(&cbor_value) {
-                Ok(error_message) => {
-                    self.result = Some(Err(error_message));
-                }
-                Err(_) => {
-                    self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data))));
+        let confirm: Value = de::from_slice(&data[..]).unwrap();
+        debug!("Confirm: {:?}", &confirm);
+        match &confirm {
+            Value::Array(confirm_vec) => {
+                match confirm_vec.get(0) {
+                    Some(msg_type) => {
+                        match msg_type {
+                            Value::Integer(msg_type_int) => {
+                                if *msg_type_int == MSG_ACCEPT_VERSION_MSG_ID {
+                                    match confirm_vec.get(1) {
+                                        Some(accepted_protocol_value) => {
+                                            match accepted_protocol_value {
+                                                Value::Integer(accepted_protocol) => {
+                                                    if *accepted_protocol < MIN_PROTOCOL_VERSION {
+                                                        self.result = Some(Err(format!("Expected protocol version {}, but was {}", MIN_PROTOCOL_VERSION, accepted_protocol)));
+                                                    } else {
+                                                        match confirm_vec.get(2) {
+                                                            Some(accepted_vec_value) => {
+                                                                match accepted_vec_value {
+                                                                    Value::Array(accepted_vec) => {
+                                                                        match accepted_vec.get(0) {
+                                                                            Some(accepted_magic_value) => {
+                                                                                match accepted_magic_value {
+                                                                                    Value::Integer(accepted_magic) => {
+                                                                                        if *accepted_magic == self.network_magic as i128 {
+                                                                                            self.result = Some(Ok(hex::encode(data)));
+                                                                                        } else {
+                                                                                            self.result = Some(Err(format!("Expected network magic {}, but was {}", self.network_magic, accepted_magic)));
+                                                                                        }
+                                                                                    }
+                                                                                    _ => { self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data)))); }
+                                                                                }
+                                                                            }
+                                                                            None => { self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data)))); }
+                                                                        }
+                                                                    }
+                                                                    _ => { self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data)))); }
+                                                                }
+                                                            }
+                                                            None => { self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data)))); }
+                                                        }
+                                                    }
+                                                }
+                                                _ => { self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data)))); }
+                                            }
+                                        }
+                                        None => { self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data)))); }
+                                    }
+                                } else {
+                                    match self.find_error_message(&confirm) {
+                                        Ok(error_message) => {
+                                            self.result = Some(Err(error_message));
+                                        }
+                                        Err(_) => { self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data)))); }
+                                    }
+                                }
+                            }
+                            _ => { self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data)))); }
+                        }
+                    }
+                    None => { self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data)))); }
                 }
             }
-        } else {
-            self.result = Some(Ok(hex::encode(data)));
+            _ => { self.result = Some(Err(format!("Unable to parse payload error! {}", hex::encode(data)))); }
         }
+
         debug!("HandshakeProtocol::State::Done");
         self.state = State::Done
     }
